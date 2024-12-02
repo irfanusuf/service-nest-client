66d3039f3e424cbda398f16520103bd3e69759e2
irfan_usuf <irfanusuf33@gmail.com>
2024-12-02 23:35:20 +0530

    addedCss

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,67 +1,61 @@
 import React, { useContext, useState } from "react";
 import "./UserProfile.css";
 import { Context } from "../context/Store";
-import IsAuthorised from "../utils/IsAuthorised";
-
-
+// import IsAuthorised from "../utils/IsAuthorised";
 
 const UserProfile = () => {
+  // IsAuthorised();
 
-
-  IsAuthorised()
-
-  const { user  , uploadProfileImage} = useContext(Context);
+  const { user, uploadProfileImage } = useContext(Context);
 
   const [image, setImage] = useState(null);
 
-  const formData = new FormData()     // empty arrray
+  const formData = new FormData(); // empty arrray
 
-  formData.append("image" , image)
-
-  const handleImage = (e)=>{
-    setImage(e.target.files[0])
-  }
-
-  const handleUpload = (formData)=>{
-
-    uploadProfileImage(formData)
-    setImage(null)
-
-  }
-
-
-
+  formData.append("image", image);
 
   return (
-
-  
-    <div className="main-Container">
+    <div className="container">
       <div className="profile-container">
-        This profile belongs to {user.username}
-
-
-
-        <img src= {user.profilepicUrl} alt={user.username}  width={200}/>
-
-
-        <h2>  user is having services {user.services && user.services[0].serviceTitle}</h2>
-
-
+        <div className="profile-pic">
+          {" "}
+          <img src={user.profilepicUrl} alt={user.username} width={200} />{" "}
+        </div>
 
         <input
           type="file"
-          multiple
           placeholder="Upload your profile "
-          onChange={(e)=>{handleImage(e)}}
+          onChange={(e) => {
+            setImage(e.target.files[0]);
+          }}
         />
-        <button type="button" onClick={()=>{handleUpload(formData)}}> Upload </button>
+        <button
+          type="button"
+          onClick={() => {
+            uploadProfileImage(formData);
+            setImage(null);
+          }}
+        >
+          Upload
+        </button>
 
+        <button className="btn">  Activate account </button>
+        <button className="btn"> Add service  </button>
+        <h2> username</h2>
 
+        <h3> Live Location </h3>
 
+        <h2>Description</h2>
 
+        <h2> Reviews and  Ratings (4)</h2>
+
+        <h2> Services Provided (3)</h2>
+
+        <h3> Report Abuse</h3>
+
+        <h2>{user.services && user.services[0].serviceTitle}</h2>
       </div>
     </div>
-  
   );
 };
 
